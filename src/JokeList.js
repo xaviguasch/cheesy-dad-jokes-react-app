0444377e1012b3d9e6f4a0fe9ccc6dceecf42538
xaviguasch <xaviguasch@gmail.com>
2019-05-10 13:17:09 +0200

    fixed localStorage bug

--- src/JokeList.js
+++ src/JokeList.js
@@ -14,6 +14,7 @@ class JokeList extends Component {
     this.state = {
       jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]')
     }
+    this.handleClick = this.handleClick.bind(this)
   }
 
   componentDidMount() {
@@ -30,17 +31,25 @@ class JokeList extends Component {
 
       jokes.push({ id: uuid(), text: res.data.joke, votes: 0 })
     }
-    this.setState({
-      jokes: jokes
-    })
-
-    window.localStorage.setItem('jokes', JSON.stringify(jokes))
+    this.setState(
+      st => ({
+        jokes: [...st.jokes, ...jokes]
+      }),
+      () => window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+    )
   }
 
   handleVote(id, delta) {
-    this.setState(st => ({
-      jokes: st.jokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j))
-    }))
+    this.setState(
+      st => ({
+        jokes: st.jokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j))
+      }),
+      () => window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+    )
+  }
+
+  handleClick() {
+    this.getJokes()
   }
 
   render() {
@@ -51,7 +60,9 @@ class JokeList extends Component {
             <span>Dad</span> Jokes
           </h1>
           <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
-          <button className='JokeList-getmore'>New Jokes</button>
+          <button className='JokeList-getmore' onClick={this.handleClick}>
+            New Jokes
+          </button>
         </div>
 
         <div className='JokeList-jokes'>
